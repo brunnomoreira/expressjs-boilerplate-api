db36705a6d97e7909ed91c58bf6debb73584950b
Brunno Moreira <brunnomoreira.silva@gmail.com>
2022-12-11 15:53:26 -0300

    fix indent

--- src/loaders/index.js
+++ src/loaders/index.js
@@ -1,9 +1,9 @@
 const express = require('./express.loader');
 
 function bootstrap(app) {
-    express.bootstrap(app);
+  express.bootstrap(app);
 }
 
 module.exports = {
-    bootstrap
+  bootstrap
 }
